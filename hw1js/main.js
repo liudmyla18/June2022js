fc0ee91777c90a26be5eb1bcc17b05f2e7f25c79
liudmyla18 <l.mykytchuk@yahoo.com>
2022-07-24 15:33:13 +0200

    homwork1js

---
+++ hw1js/main.js
@@ -0,0 +1,69 @@
+let a = ` hello `;
+console.log(a);
+alert(a);
+document.write(a);
+let b = ` owu `;
+console.log(b);
+alert(b);
+document.write(b);
+let c = ` com`;
+console.log(c);
+alert(c);
+document.write(c);
+let d =` ua`;
+console.log(d);
+alert(d);
+document.write(d);
+let numb = ( 1);
+console.log(numb);
+alert(numb);
+document.write( numb );
+let numb2 = (10);
+console.log(numb2);
+alert(numb2);
+document.write(numb2);
+let numb3 = (-999);
+console.log(numb3);
+alert(numb3);
+document.write(numb3);
+let numb4= 123;
+console.log(numb4);
+alert(numb4);
+document.write(numb4);
+let numb5 = 3.14;
+console.log(numb5);
+alert(numb5);
+document.write(numb5);
+let numb6 = 2.7;
+console.log(numb6);
+alert(numb6);
+document.write(numb6);
+let numb7 = 16;
+console.log(numb7);
+alert(numb7);
+document.write(numb7);
+let numb8 =  true;
+console.log(numb8);
+alert(numb8);
+document.write(numb8);
+let numb9 =  false;
+console.log(numb9);
+alert(numb9);
+document.write(numb9);
+let firstName = ` Mykytchuk`;
+let middleName =  ` liudmyla`;
+let lastName = ` Ivanivna`;
+let person = ( firstName+ middleName+lastName);
+console.log( person);
+document.write( person);
+let aa = 100;
+let bb = '100';
+let cc = true;
+console.log(typeof aa,bb,cc );
+
+prompt(` Ivanivna`)
+prompt(` liudmyla`)
+prompt(`18.08`)
+console.log(prompt)
+
+
